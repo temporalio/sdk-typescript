8a96d65d199932b74760cb728afaf3c304a35c31
Roey Berman <roey.berman@gmail.com>
2021-01-26 10:59:35 +0200

    Try wrapping root callback with Arc

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,12 +1,11 @@
 mod mock_core;
 
-use neon::prelude::*;
-
+use ::neon::prelude::*;
+use ::neon::register_module;
 use ::std::cell::RefCell;
 use ::temporal_sdk_core::protos::coresdk::poll_sdk_task_resp::Task::WfTask;
 use ::temporal_sdk_core::protos::coresdk::PollSdkTaskResp;
 use ::temporal_sdk_core::protos::coresdk::*;
-use neon::register_module;
 
 type BoxedWorker = JsBox<RefCell<Worker>>;
 
@@ -65,21 +64,37 @@ fn worker_poll(mut cx: FunctionContext) -> JsResult<JsUndefined> {
     let worker = cx.argument::<BoxedWorker>(0)?;
     let callback = cx.argument::<JsFunction>(1)?.root(&mut cx);
     let mut worker = worker.borrow_mut().clone();
+    let arc_callback = ::std::sync::Arc::new(callback);
     let queue = cx.queue();
     std::thread::spawn(move || loop {
+        let arc_callback = arc_callback.clone();
         let response_option = worker.poll();
+        if let Err(_) = response_option {
+            // For mock core this signals end of tasks
+            queue.send(move |mut cx| {
+                let raw_callback = ::std::sync::Arc::into_raw(arc_callback);
+                if let Some(r) = unsafe { raw_callback.as_ref() } {
+                    r.drop(&mut cx);
+                };
+                Ok(())
+            });
+            break;
+        }
         queue.send(move |mut cx| {
-            let callback = callback.into_inner(&mut cx);
-            let this = cx.undefined();
-            let args: Vec<Handle<JsValue>> = match response_option {
-                Ok(response) => {
-                    let error = cx.undefined();
-                    let result = poll_sdk_task_resp_to_js_object(&mut cx, &response)?;
-                    vec![error.upcast(), result.upcast()]
-                }
-                Err(_) => vec![cx.empty_object().upcast(), cx.undefined().upcast()],
+            let raw_callback = ::std::sync::Arc::into_raw(arc_callback);
+            if let Some(r) = unsafe { raw_callback.as_ref() } {
+                let callback = r.clone(&mut cx).into_inner(&mut cx);
+                let this = cx.undefined();
+                let args: Vec<Handle<JsValue>> = match response_option {
+                    Ok(response) => {
+                        let error = cx.undefined();
+                        let result = poll_sdk_task_resp_to_js_object(&mut cx, &response)?;
+                        vec![error.upcast(), result.upcast()]
+                    }
+                    Err(_) => vec![cx.empty_object().upcast(), cx.undefined().upcast()],
+                };
+                callback.call(&mut cx, this, args)?;
             };
-            callback.call(&mut cx, this, args)?;
             Ok(())
         });
     });
--- src/tryNative.ts
+++ src/tryNative.ts
@@ -1,43 +1,19 @@
-import { promisify } from 'util';
-import { newWorker, workerPoll, PollCallback } from '../native';
+// import { promisify } from 'util';
+import { newWorker, workerPoll } from '../native';
 import { Workflow } from './engine';
 import * as stdlib from './stdlib';
 
 async function run() {
   const worker = newWorker("tasks");
-  const scriptName = process.argv[process.argv.length - 1];
-  const poll = promisify((callback: PollCallback) => workerPoll(worker, callback));
+  // const scriptName = process.argv[process.argv.length - 1];
+  // const poll = promisify((callback: PollCallback) => workerPoll(worker, callback));
 
   // Only a single workflow at the moment
   const workflow = await Workflow.create('TODO');
   await stdlib.install(workflow);
-
-  while (true) {
-    const res = await poll();
-    if (res === undefined) {
-      console.error('Unexpected undefined poll result');
-      return;
-    }
-    if (Object.keys(res).length === 0) {
-      console.log('done');
-      return;
-    }
-    console.log(res);
-    switch (res.type) {
-      case 'StartWorkflow': {
-        const commands = await workflow.runMain(scriptName);
-        console.log(commands[0]);
-        break;
-      }
-      case 'CompleteTimer': {
-        const commands = await workflow.trigger(res);
-        console.log(commands[0]);
-        break;
-      }
-      default:
-        // ignore
-    }
-  }
+  workerPoll(worker, (err, result) => {
+    console.log('poll', { err, result });
+  });
 }
 
 run().catch((err) => {
