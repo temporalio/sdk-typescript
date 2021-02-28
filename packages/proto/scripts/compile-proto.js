2796f7e2fef1f50be52e0af7011ea2eb90e5cc83
Roey Berman <roey.berman@gmail.com>
2021-02-28 13:14:52 +0200

    Fix proto build

--- packages/proto/scripts/compile-proto.js
+++ packages/proto/scripts/compile-proto.js
@@ -1,7 +1,10 @@
+const { promisify } = require('util');
 const { resolve } = require('path');
 const { spawnSync } = require('child_process');
 const dedent = require('dedent');
 const { removeSync, mkdirsSync, readFileSync, writeFileSync } = require('fs-extra');
+const pbjs = require('protobufjs/cli/pbjs');
+const pbts = require('protobufjs/cli/pbts');
 
 const outputDir = resolve(__dirname, '..');
 const commonjsOutputDir = resolve(outputDir, 'commonjs');
@@ -12,16 +15,7 @@ removeSync(es2020OutputDir);
 mkdirsSync(commonjsOutputDir);
 mkdirsSync(es2020OutputDir);
 
-function npx(...args) {
-  console.log(...args);
-  const { status } = spawnSync('npx', args, { stdio: 'inherit' });
-  if (status !== 0) {
-    throw new Error(`Failed to run ${args[0]}`);
-  }
-}
-
 const pbjsArgs = (wrap, out) => [
-  'pbjs',
   '--path',
   resolve(protoBaseDir, 'api_upstream'),
   '--wrap',
@@ -35,23 +29,34 @@ const pbjsArgs = (wrap, out) => [
   resolve(protoBaseDir, 'api_upstream/temporal/api/workflowservice/v1/service.proto'),
 ];
 
-console.log('Creating protobuf JS definitions');
-npx(...pbjsArgs('commonjs', resolve(commonjsOutputDir, 'index.js')));
+async function main() {
+  console.log('Creating protobuf JS definitions');
+  await promisify(pbjs.main)(pbjsArgs('commonjs', resolve(commonjsOutputDir, 'index.js')));
 
-console.log('Creating protobuf TS definitions');
-npx('pbts', '--out', resolve(commonjsOutputDir, 'index.d.ts'), resolve(commonjsOutputDir, 'index.js'));
+  console.log('Creating protobuf TS definitions');
+  await promisify(pbts.main)([
+    '--out',
+    resolve(commonjsOutputDir, 'index.d.ts'),
+    resolve(commonjsOutputDir, 'index.js'),
+  ]);
 
-console.log('Converting protobufjs/minimal to ES module for isolate import');
-const protobufjsSource = readFileSync(require.resolve('protobufjs/dist/minimal/protobuf.js'));
-writeFileSync(
-  resolve(es2020OutputDir, 'protobuf.js'),
-  dedent`
+  console.log('Converting protobufjs/minimal to ES module for isolate import');
+  const protobufjsSource = readFileSync(require.resolve('protobufjs/dist/minimal/protobuf.js'));
+  writeFileSync(
+    resolve(es2020OutputDir, 'protobuf.js'),
+    dedent`
   const module = { exports: {} }; 
   ${protobufjsSource}
   const { Reader, Writer, util, roots, rpc } = module.exports;
   export { Reader, Writer, util, roots, rpc };
   `
-);
+  );
 
-console.log('Create protobuf JS definitions for isolates');
-npx(...pbjsArgs('es6', resolve(es2020OutputDir, 'index.js')));
+  console.log('Create protobuf JS definitions for isolates');
+  await promisify(pbjs.main)(pbjsArgs('es6', resolve(es2020OutputDir, 'index.js')));
+}
+
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
