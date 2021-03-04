1c37003448428455dbee96f4520561c853aa3bbe
Roey Berman <roey.berman@gmail.com>
2021-03-04 14:53:32 +0200

    Organize worker code

--- packages/worker/native/index.d.ts
+++ packages/worker/native/index.d.ts
@@ -4,7 +4,7 @@ export declare type PollCallback = (err?: Error, result?: ArrayBuffer) => void;
 export declare function newWorker(): Worker;
 export declare function workerShutdown(worker: Worker): void;
 export declare function workerPoll(worker: Worker, queueName: string, callback: PollCallback): void;
-export declare function workerCompleteTask(worker: Worker, result: ArrayBuffer): boolean;
+export declare function workerCompleteTask(worker: Worker, result: ArrayBuffer): void;
 export declare function workerSuspendPolling(worker: Worker): void;
 export declare function workerResumePolling(worker: Worker): void;
 export declare function workerIsSuspended(worker: Worker): boolean;
