eaa559c7bd21cb83553a927ff6dce2581b8ff4cf
Roey Berman <roey.berman@gmail.com>
2021-03-14 09:58:33 +0200

    Add activity package

---
+++ packages/activity/src/index.ts
@@ -0,0 +1,5 @@
+export class Context {
+  public static current(): Context {
+    throw new Error('Not implemented');
+  }
+}
--- packages/test-activities/src/index.ts
+++ packages/test-activities/src/index.ts
@@ -1,7 +1,13 @@
+import { Context } from '@temporalio/activity';
+
 export async function httpGet(url: string): Promise<string> {
   return `<html><body>hello from ${url}</body></html>`;
 }
 
 export async function throwAnError(message: string): Promise<void> {
   throw new Error(message);
+}
+
+export async function waitForCancellation(): Promise<void> {
+  throw new Error('Not implemented');
 }
