ea0f60fc09c67725acaeb3fe2cf2fbecc3827418
Roey Berman <roey.berman@gmail.com>
2021-03-14 09:43:08 +0200

    Test activity failure

--- packages/test-activities/src/index.ts
+++ packages/test-activities/src/index.ts
@@ -1,3 +1,7 @@
 export async function httpGet(url: string): Promise<string> {
   return `<html><body>hello from ${url}</body></html>`;
 }
+
+export async function throwAnError(message: string): Promise<void> {
+  throw new Error(message);
+}
