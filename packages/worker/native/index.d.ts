3d906077d4514a018e672ca1d221795f91183814
Roey Berman <roey.berman@gmail.com>
2021-03-18 12:00:56 +0200

    Address CR comments

--- packages/worker/native/index.d.ts
+++ packages/worker/native/index.d.ts
@@ -1,6 +1,6 @@
 export interface Worker {}
 
-export declare type PollCallback = (err?: Error, result?: ArrayBuffer) => void;
+export declare type PollCallback = (err?: Error, result: ArrayBuffer) => void;
 export declare function newWorker(): Worker;
 export declare function workerShutdown(worker: Worker): void;
 export declare function workerPoll(worker: Worker, queueName: string, callback: PollCallback): void;
--- packages/worker/native/src/lib.rs
+++ packages/worker/native/src/lib.rs
@@ -3,7 +3,7 @@ use prost::Message;
 use std::{
     convert::TryInto,
     sync::{
-        mpsc::{sync_channel, SyncSender},
+        mpsc::{sync_channel, Receiver, SyncSender},
         Arc,
     },
     time::Duration,
@@ -16,20 +16,25 @@ use temporal_sdk_core::{
 
 type BoxedWorker = JsBox<Arc<Worker>>;
 
-pub struct PollItem {
+/// A request from lang to poll a queue
+pub struct PollRequest {
+    /// Name of queue to poll
     queue_name: String,
+    /// Used to send the poll result back into lang
     callback: Root<JsFunction>,
 }
 
 pub struct Worker {
     core: Box<dyn Core + Send + Sync>,
-    sender: SyncSender<PollItem>,
+    sender: SyncSender<PollRequest>,
 }
 
 impl Finalize for Worker {}
 
 impl Worker {
-    pub fn new(sender: SyncSender<PollItem>) -> Self {
+    pub fn new() -> (Self, Receiver<PollRequest>) {
+        // Set capacity to 1 because we only poll from a single thread
+        let (sender, receiver) = sync_channel::<PollRequest>(1);
         let core = init(CoreInitOptions {
             gateway_opts: ServerGatewayOptions {
                 target_url: "http://localhost:7233".try_into().unwrap(),
@@ -41,10 +46,12 @@ impl Worker {
         })
         .unwrap();
 
-        Worker {
+        let worker = Worker {
             core: Box::new(core),
             sender,
-        }
+        };
+
+        (worker, receiver)
     }
 
     pub fn poll(&self, queue_name: String) -> ::temporal_sdk_core::Result<coresdk::Task> {
@@ -52,63 +59,72 @@ impl Worker {
     }
 }
 
+// Below are functions exported to JS
+
+/// Create a new worker.
+/// Immediately spawns a poller thread that will block on [PollRequest]s
 fn worker_new(mut cx: FunctionContext) -> JsResult<BoxedWorker> {
-    // TODO: is capacity of 1 enough here?
-    let (sender, receiver) = sync_channel::<PollItem>(1);
-    let worker = Arc::new(Worker::new(sender));
+    let (worker, receiver) = Worker::new();
+    let worker = Arc::new(worker);
     let queue = cx.queue();
     let cloned_worker = Arc::clone(&worker);
 
-    std::thread::spawn(move || {
-        let worker = cloned_worker;
-        loop {
-            let item = receiver.recv().unwrap();
-            let queue_name = item.queue_name;
-            let callback = item.callback;
-            let result = worker.poll(queue_name);
-            match result {
-                Ok(task) => {
-                    queue.send(move |mut cx| {
-                        let callback = callback.into_inner(&mut cx);
-                        let this = cx.undefined();
-                        let error = cx.undefined();
-                        let len = task.encoded_len();
-                        let mut result = JsArrayBuffer::new(&mut cx, len as u32)?;
-                        cx.borrow_mut(&mut result, |data| {
-                            let mut slice = data.as_mut_slice::<u8>();
-                            if let Err(_) = task.encode(&mut slice) {
-                                panic!("Failed to encode task")
-                            };
-                        });
-                        let args: Vec<Handle<JsValue>> = vec![error.upcast(), result.upcast()];
-                        callback.call(&mut cx, this, args)?;
-                        Ok(())
+    std::thread::spawn(move || loop {
+        let item = receiver.recv().unwrap();
+        let queue_name = item.queue_name;
+        let callback = item.callback;
+        let result = worker.poll(queue_name);
+        match result {
+            Ok(task) => {
+                queue.send(move |mut cx| {
+                    let callback = callback.into_inner(&mut cx);
+                    let this = cx.undefined();
+                    let error = cx.undefined();
+                    let len = task.encoded_len();
+                    let mut result = JsArrayBuffer::new(&mut cx, len as u32)?;
+                    cx.borrow_mut(&mut result, |data| {
+                        let mut slice = data.as_mut_slice::<u8>();
+                        if let Err(_) = task.encode(&mut slice) {
+                            panic!("Failed to encode task")
+                        };
                     });
-                }
-                Err(err) => {
-                    queue.send(move |mut cx| {
-                        let callback = callback.into_inner(&mut cx);
-                        let this = cx.undefined();
-                        let error = JsError::error(&mut cx, format!("{}", err))?;
-                        let result = cx.undefined();
-                        let args: Vec<Handle<JsValue>> = vec![error.upcast(), result.upcast()];
-                        callback.call(&mut cx, this, args)?;
-                        Ok(())
-                    });
+                    let args: Vec<Handle<JsValue>> = vec![error.upcast(), result.upcast()];
+                    callback.call(&mut cx, this, args)?;
+                    Ok(())
+                });
+            }
+            Err(err) => {
+                // TODO: on the JS side we consider all errors fatal, revise this later
+                let should_break = match err {
+                    temporal_sdk_core::CoreError::ShuttingDown => true,
+                    _ => false,
+                };
+                queue.send(move |mut cx| {
+                    let callback = callback.into_inner(&mut cx);
+                    let this = cx.undefined();
+                    let error = JsError::error(&mut cx, format!("{}", err))?;
+                    let result = cx.undefined();
+                    let args: Vec<Handle<JsValue>> = vec![error.upcast(), result.upcast()];
+                    callback.call(&mut cx, this, args)?;
+                    Ok(())
+                });
+                if should_break {
                     break;
                 }
             }
         }
     });
 
-    Ok(cx.boxed(worker))
+    Ok(cx.boxed(cloned_worker))
 }
 
+/// Initiate a single poll request.
+/// Will block if a poll request is already in-flight
 fn worker_poll(mut cx: FunctionContext) -> JsResult<JsUndefined> {
     let worker = cx.argument::<BoxedWorker>(0)?;
     let queue_name = cx.argument::<JsString>(1)?.value(&mut cx);
     let callback = cx.argument::<JsFunction>(2)?.root(&mut cx);
-    let item = PollItem {
+    let item = PollRequest {
         queue_name,
         callback,
     };
@@ -121,6 +137,7 @@ fn worker_poll(mut cx: FunctionContext) -> JsResult<JsUndefined> {
     }
 }
 
+/// Submit a task completion to core.
 fn worker_complete_task(mut cx: FunctionContext) -> JsResult<JsUndefined> {
     let worker = cx.argument::<BoxedWorker>(0)?;
     let completion = cx.argument::<JsArrayBuffer>(1)?;
@@ -141,6 +158,9 @@ fn worker_complete_task(mut cx: FunctionContext) -> JsResult<JsUndefined> {
     }
 }
 
+/// Request shutdown of the worker.
+/// Caller should wait until a [CoreError::ShuttingDown] is returned from poll to ensure graceful
+/// shutdown.
 fn worker_shutdown(mut cx: FunctionContext) -> JsResult<JsUndefined> {
     let worker = cx.argument::<BoxedWorker>(0)?;
     match worker.core.shutdown() {
