bec12565eb885c75cddf0d73d28dbd77725209a5
Roey Berman <roey.berman@gmail.com>
2021-04-22 20:02:49 +0300

    Publish API reference (#70)
    * Build and deploy docs in CI
    
    * Add poor man's snipsync
    
    * Add target to snipsync log
    
    * Use docusaurus-plugin-snipsync
    
    * Ignore docusaurus generated files in prettier
    
    * Reinstall dependencies (update package-lock.json)
    
    * Only publish docs on main branch

---
+++ packages/docs/README.md
@@ -0,0 +1,15 @@
+# `@temporalio/docs`
+
+Generates the API reference docs using [Docusaurus 2](https://docusaurus.io/), a modern static website generator.
+
+## Local Development
+
+```console
+npm start
+```
+
+This command starts a local development server and opens up a browser window. Most changes are reflected live without having to restart the server.
+
+## Deployment
+
+The reference is built and deployed to Netlify under https://nodejs.temporal.io during CI for branch `main` using GitHub Actions.
---
+++ packages/docs/docusaurus.config.js
@@ -0,0 +1,130 @@
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: 'Temporal NodeJS SDK API Reference',
+  tagline: 'Build invincible applications',
+  url: 'https://nodejs.temporal.io',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'temporalio',
+  projectName: 'sdk-node',
+  themeConfig: {
+    prism: {
+      theme: require('prism-react-renderer/themes/dracula'),
+    },
+    navbar: {
+      logo: {
+        alt: 'Temporal',
+        src: 'img/temporal-logo-dark.svg',
+        srcDark: 'img/temporal-logo.svg',
+      },
+      items: [
+        {
+          href: 'https://github.com/temporalio/sdk-node',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          items: [
+            {
+              label: 'NPM',
+              href: 'https://www.npmjs.com/package/temporalio',
+            },
+            {
+              label: 'Support Forum',
+              href: 'https://community.temporal.io/',
+            },
+            {
+              label: 'Public Slack',
+              href: 'https://join.slack.com/t/temporalio/shared_invite/zt-onhti57l-J0bl~Tr7MqSUnIc1upjRkw',
+            },
+            {
+              label: 'Temporal Careers',
+              href: 'https://temporal.io/careers',
+            },
+          ],
+        },
+        {
+          items: [
+            {
+              label: 'Temporal Documentation',
+              to: 'https://docs.temporal.io',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/temporalio/sdk-node',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/temporalio',
+            },
+            {
+              label: 'YouTube',
+              href: 'https://www.youtube.com/channel/UCGovZyy8OfFPNlNV0i1fI1g',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Temporal Technologies Inc.`,
+    },
+    gtag: {
+      trackingID: 'UA-163137879-1',
+      // Optional fields.
+      anonymizeIP: true, // Should IPs be anonymized?
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          routeBasePath: '/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+        sitemap: {
+          changefreq: 'weekly',
+          priority: 0.5,
+        },
+      },
+    ],
+  ],
+  plugins: [
+    [
+      'docusaurus-plugin-typedoc',
+      {
+        entryPoints: ['../meta/src/index.ts'],
+        tsconfig: '../meta/tsconfig.json',
+        excludePrivate: true,
+        excludeProtected: true,
+        hideGenerator: true,
+        disableSources: true,
+        hideBreadcrumbs: true,
+        readme: 'none',
+        watch: ['y', 'yes', 't', 'true', '1'].includes(process.env.TYPEDOC_WATCH),
+      },
+    ],
+    [
+      'docusaurus-plugin-snipsync',
+      {
+        origins: [
+          {
+            files: ['../*/src/*.ts', '../create-project/samples/*.ts'],
+          },
+        ],
+        target: 'docs',
+        features: {
+          enable_source_link: false,
+        },
+      },
+    ],
+  ],
+};
