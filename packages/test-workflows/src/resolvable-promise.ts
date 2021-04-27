d72ef4b7e62eb26ce5384ca4869ca852d378262b
Roey Berman <roey.berman@gmail.com>
2021-04-27 20:22:06 +0300

    Comment about ResolvablePromise WF module

--- packages/test-workflows/src/resolvable-promise.ts
+++ packages/test-workflows/src/resolvable-promise.ts
@@ -1,8 +1,12 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 
 /**
- * Helper for creating promises which can be manually resolved
- * Copied from workflow/common
+ * Helper for creating promises which can be manually resolved.
+ * Copied from workflow/common because of a limitation of the module loader
+ * which doesn't inject it into the workflow runtime.
+ *
+ * TODO(bergundy): Export a transferable ResolvablePromise that considers
+ * scopes and cancellation.
  */
 export class ResolvablePromise<T> implements PromiseLike<T> {
   public readonly then: <TResult1 = T, TResult2 = never>(
