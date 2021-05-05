5b226efb98e4d365303c72a9edbe22cd7ab82ae9
Roey Berman <roey.berman@gmail.com>
2021-05-05 14:52:10 +0300

    WIP: Bundle on worker creation

--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  entry: ['./packages/workflow/es2020/init.js', './packages/test-workflows/lib/sync.js'],
-  mode: 'development',
-  output: {
-    library: 'lib',
-    filename: '[name].js',
-  },
-};
