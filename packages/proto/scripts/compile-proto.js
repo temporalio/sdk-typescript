208e83f7f206c424251d7923072df156c626f494
Roey Berman <roey.berman@gmail.com>
2021-05-06 13:04:40 +0300

    Remove esm build for proto package

--- packages/proto/scripts/compile-proto.js
+++ packages/proto/scripts/compile-proto.js
@@ -1,31 +1,30 @@
-const { promisify } = require('util');
 const { resolve } = require('path');
+const { promisify } = require('util');
 const dedent = require('dedent');
 const glob = require('glob');
-const { statSync, removeSync, mkdirsSync, readFileSync, writeFileSync } = require('fs-extra');
+const { statSync, mkdirsSync, readFileSync, writeFileSync } = require('fs-extra');
 const pbjs = require('protobufjs/cli/pbjs');
 const pbts = require('protobufjs/cli/pbts');
 
-const outputDir = resolve(__dirname, '..');
-const commonjsOutputDir = resolve(outputDir, 'commonjs');
-const es2020OutputDir = resolve(outputDir, 'es2020');
+const outputDir = resolve(__dirname, '../lib');
+const jsOutputFile = resolve(outputDir, 'index.js');
+const dtsOutputFile = resolve(outputDir, 'index.d.ts');
 const protoBaseDir = resolve(__dirname, '../../worker/native/sdk-core/protos');
-mkdirsSync(commonjsOutputDir);
-mkdirsSync(es2020OutputDir);
+mkdirsSync(outputDir);
 
 const coreProtoPath = resolve(protoBaseDir, 'local/core_interface.proto');
 const serviceProtoPath = resolve(protoBaseDir, 'api_upstream/temporal/api/workflowservice/v1/service.proto');
 
-const pbjsArgs = (wrap, out) => [
+const pbjsArgs = [
   '--path',
   resolve(protoBaseDir, 'api_upstream'),
   '--wrap',
-  wrap,
+  'commonjs',
   '--target',
   'static-module',
   '--force-long',
   '--out',
-  out,
+  jsOutputFile,
   coreProtoPath,
   serviceProtoPath,
 ];
@@ -43,30 +42,24 @@ function mtime(path) {
 
 async function main() {
   const protoFiles = glob.sync(resolve(protoBaseDir, '**/*.proto'));
-  // TODO: any proto file could have changed
   const protosMTime = Math.max(...protoFiles.map(mtime));
 
-  const commonjsImplPath = resolve(commonjsOutputDir, 'index.js');
-  if (protosMTime < mtime(commonjsImplPath)) {
+  if (protosMTime < mtime(jsOutputFile)) {
     console.log('Asuming protos are up to date');
     return;
   }
 
   console.log('Creating protobuf JS definitions');
-  await promisify(pbjs.main)(pbjsArgs('commonjs', commonjsImplPath));
+  await promisify(pbjs.main)(pbjsArgs);
 
   console.log('Creating protobuf TS definitions');
-  await promisify(pbts.main)([
-    '--out',
-    resolve(commonjsOutputDir, 'index.d.ts'),
-    resolve(commonjsOutputDir, 'index.js'),
-  ]);
+  await promisify(pbts.main)(['--out', dtsOutputFile, jsOutputFile]);
 
   const moduleHeader = readFileSync(resolve(__dirname, 'module-header.txt'), 'utf8');
   // Prepend module docs and fix issue where Long is not found in TS definitions (https://github.com/protobufjs/protobuf.js/issues/1533)
-  const pbtsOutput = readFileSync(resolve(commonjsOutputDir, 'index.d.ts'), 'utf8');
+  const pbtsOutput = readFileSync(dtsOutputFile, 'utf8');
   writeFileSync(
-    resolve(commonjsOutputDir, 'index.d.ts'),
+    dtsOutputFile,
     dedent`
   ${moduleHeader}
   import Long from "long";
@@ -74,20 +67,6 @@ async function main() {
   `
   );
 
-  console.log('Converting protobufjs/minimal to ES module for isolate import');
-  const protobufjsSource = readFileSync(require.resolve('protobufjs/dist/minimal/protobuf.js'));
-  writeFileSync(
-    resolve(es2020OutputDir, 'protobuf.js'),
-    dedent`
-  const module = { exports: {} }; 
-  ${protobufjsSource}
-  const { Reader, Writer, util, roots, rpc, configure } = module.exports;
-  export { Reader, Writer, util, roots, rpc, configure };
-  `
-  );
-
-  console.log('Creating protobuf JS definitions for isolates');
-  await promisify(pbjs.main)(pbjsArgs('es6', resolve(es2020OutputDir, 'index.js')));
   console.log('Done');
 }
 
