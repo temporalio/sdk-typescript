62de72b182c4ca41659d73e6a2812a3bb357cc28
Roey Berman <roey.berman@gmail.com>
2021-05-20 15:46:13 +0300

    chore: Customize commitlint config and fix lerna version message

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,5 +2,12 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'subject-case': [2, 'always', 'sentence-case'],
+    'scope-enum': [
+      2,
+      'always',
+      ['activity', 'bench', 'client', 'docs', 'core', 'release', 'samples', 'worker', 'workflow'],
+    ],
+    'header-max-length': [2, 'always', 120],
+    'footer-max-line-length': [2, 'always', 120],
   },
 };
