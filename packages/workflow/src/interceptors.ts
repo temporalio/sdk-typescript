58ccc9885a3b8bce937495de1e5585ae9dcfd7b1
Roey Berman <roey.berman@gmail.com>
2021-06-10 09:08:14 +0300

    chore: Address CR comments

--- packages/workflow/src/interceptors.ts
+++ packages/workflow/src/interceptors.ts
@@ -10,7 +10,11 @@ import { coresdk } from '@temporalio/proto';
 import { ActivityOptions } from './interfaces';
 import { AnyFunc, OmitLastParam } from './type-helpers';
 
-/** Type of the next function for a given interceptor function */
+/**
+ * Type of the next function for a given interceptor function
+ *
+ * Called from an interceptor to continue the interception chain
+ */
 export type Next<IF, FN extends keyof IF> = Required<IF>[FN] extends AnyFunc ? OmitLastParam<Required<IF>[FN]> : never;
 
 /** Headers are just a mapping of header name to Payload */
@@ -28,7 +32,8 @@ export function composeInterceptors<I, M extends keyof I>(interceptors: I[], met
     const interceptor = interceptors[i];
     if (interceptor[method] !== undefined) {
       const prev = next;
-      // We loose type safety here because Typescript can't deduce that interceptor[method] is of type Next<I, M>
+      // We loose type safety here because Typescript can't deduce that interceptor[method] is a function that returns
+      // the same type as Next<I, M>
       next = ((input: any) => (interceptor[method] as any)(input, prev)) as any;
     }
   }
