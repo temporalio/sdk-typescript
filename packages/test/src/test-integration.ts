8172aceca6696cdaba03c2f2f8ed7475ed3881ff
Valeri Karpov <val@karpov.io>
2021-06-11 12:55:04 -0400

    test: Update error message in test case

--- packages/test/src/test-integration.ts
+++ packages/test/src/test-integration.ts
@@ -64,7 +64,7 @@ if (RUN_INTEGRATION_TESTS) {
     const client = new Connection();
     const workflow = client.workflow<Empty>('not-found', { taskQueue: 'test' });
     await t.throwsAsync(() => workflow.start(), {
-      message: 'Workflow not found',
+      message: 'Workflow not found: "not-found"',
       instanceOf: WorkflowExecutionFailedError,
     });
   });
