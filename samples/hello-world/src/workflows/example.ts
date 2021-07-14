23ae6319240603876b8020b6500c2e4210b684e0
Valeri Karpov <val@karpov.io>
2021-07-14 12:30:14 -0400

    chore(samples): Add hello world sample

---
+++ samples/hello-world/src/workflows/example.ts
@@ -0,0 +1,12 @@
+// @@@SNIPSTART nodejs-hello-workflow
+import { Example } from '../interfaces/workflows';
+import { greet } from '../activities/greeter';
+
+// A workflow that simply calls an activity
+async function main(name: string): Promise<string> {
+  return greet(name);
+}
+
+// Declare the workflow's type to be checked by the Typescript compiler
+export const workflow: Example = { main };
+// @@@SNIPEND
