cee55488d5fd32c87d721a4a80dee9aa755d5b3c
Roey Berman <roey.berman@gmail.com>
2021-08-26 17:34:23 +0300

    docs: Fix links to Workflow versioning docs

--- packages/workflow/src/workflow.ts
+++ packages/workflow/src/workflow.ts
@@ -683,7 +683,7 @@ export function uuid4(): string {
 /**
  * Patch or upgrade workflow code by checking or stating that this workflow has a certain patch.
  *
- * TODO: See docs page for info
+ * See [docs page](https://docs.temporal.io/docs/node/versioning) for info.
  *
  * If the workflow is replaying an existing history, then this function returns true if that
  * history was produced by a worker which also had a `patched` call with the same `patchId`.
@@ -704,7 +704,7 @@ export function patched(patchId: string): boolean {
 /**
  * Indicate that a patch is being phased out.
  *
- * TODO: See docs page, how to run queries, etc.
+ * See [docs page](https://docs.temporal.io/docs/node/versioning) for info.
  *
  * Workflows with this call may be deployed alongside workflows with a {@link patched} call, but
  * they must *not* be deployed while any workers still exist running old code without a
