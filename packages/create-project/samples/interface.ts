6877fb3f0d6dc7decbe16a60333376588f6cadc1
Loren 🤓 <lorensr@gmail.com>
2021-09-05 20:12:56 -0400

    feat(samples): Move examples to samples-node

--- packages/create-project/samples/interface.ts
+++
@@ -1,9 +0,0 @@
-// @@@SNIPSTART nodejs-hello-workflow-interface
-import { Workflow } from '@temporalio/workflow';
-
-// Extend the generic Workflow interface to check that Example is a valid workflow interface
-// Workflow interfaces are useful for generating type safe workflow clients
-export interface Example extends Workflow {
-  main(name: string): Promise<string>;
-}
-// @@@SNIPEND
