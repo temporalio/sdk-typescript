8d03e59d2d96dee85e7ade9dd4f61b1ea8adf0a0
Loren 🤓 <lorensr@gmail.com>
2021-09-06 19:47:22 -0400

    chore(samples): Improve exec error handling

--- packages/create-project/src/helpers/should-use-yarn.ts
+++ packages/create-project/src/helpers/should-use-yarn.ts
@@ -1,14 +1,21 @@
 import { execSync } from 'child_process';
 
+const COMMAND_NOT_FOUND_SH_CODE = 127;
+
 export function shouldUseYarn(): boolean {
   try {
     const userAgent = process.env.npm_config_user_agent;
     if (userAgent) {
       return Boolean(userAgent && userAgent.startsWith('yarn'));
     }
+
     execSync('yarnpkg --version', { stdio: 'ignore' });
     return true;
-  } catch (e) {
-    return false;
+  } catch (e: any) {
+    if (e.status === COMMAND_NOT_FOUND_SH_CODE || /not found/i.test(e.message)) {
+      return false;
+    }
+
+    throw e;
   }
 }
