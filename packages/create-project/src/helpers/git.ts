04ef5b766cdd9a5be3308c8183e46d20f0218bde
Loren 🤓 <lorensr@gmail.com>
2021-09-06 21:41:46 -0400

    refactor(samples): Improve git and hg error handling

--- packages/create-project/src/helpers/git.ts
+++ packages/create-project/src/helpers/git.ts
@@ -4,20 +4,37 @@ import { execSync } from 'child_process';
 import path from 'path';
 import rimraf from 'rimraf';
 
+const NOT_A_GIT_REPOSITORY_STATUS_CODE = 128;
+
 function isInGitRepository(): boolean {
   try {
     execSync('git rev-parse --is-inside-work-tree', { stdio: 'ignore' });
     return true;
-  } catch (_) {}
-  return false;
+  } catch (error: any) {
+    if (error.status === NOT_A_GIT_REPOSITORY_STATUS_CODE) {
+      return false;
+    } else {
+      // Unknown error. To be safe, assume we're in a repo.
+      return true;
+    }
+  }
 }
+
+const HG_ERROR_STATUS_CODE = 255;
 
 function isInMercurialRepository(): boolean {
   try {
     execSync('hg --cwd . root', { stdio: 'ignore' });
     return true;
-  } catch (_) {}
-  return false;
+  } catch (error: any) {
+    // There isn't anything more specific about `error` that we can pattern match against
+    if (error.status === HG_ERROR_STATUS_CODE && /Command failed/.test(error.message)) {
+      return false;
+    } else {
+      // Unknown error. To be safe, assume we're in a repo.
+      return true;
+    }
+  }
 }
 
 export function tryGitInit(root: string): boolean {
