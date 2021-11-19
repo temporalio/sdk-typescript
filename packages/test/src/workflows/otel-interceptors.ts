b6e2e5aa162c68f5c72a1837b80a2fdff7368e36
Roey Berman <roey.berman@gmail.com>
2021-11-19 10:01:59 -0600

    Cleanup otel interceptors test workflow and unused sample

--- packages/test/src/workflows/otel-interceptors.ts
+++ packages/test/src/workflows/otel-interceptors.ts
@@ -4,9 +4,11 @@ import { WorkflowInterceptors } from '@temporalio/workflow';
 import {
   OpenTelemetryInboundInterceptor,
   OpenTelemetryOutboundInterceptor,
+  OpenTelemetryInternalsInterceptor,
 } from '@temporalio/interceptors-opentelemetry/lib/workflow';
 
 export const interceptors = (): WorkflowInterceptors => ({
   inbound: [new OpenTelemetryInboundInterceptor()],
   outbound: [new OpenTelemetryOutboundInterceptor()],
+  internals: [new OpenTelemetryInternalsInterceptor()],
 });
