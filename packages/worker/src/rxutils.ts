abb40b0dd2f8fedee643df8245d5562beed6b15f
Josh Goldberg <me@joshuakgoldberg.com>
2022-01-13 02:54:08 -0500

    Added ts-prune to detect unused code (#405)

--- packages/worker/src/rxutils.ts
+++ packages/worker/src/rxutils.ts
@@ -6,20 +6,9 @@ interface StateAndOptionalOutput<T, O> {
   output?: O;
 }
 
-export type StateAndOutput<T, O> = Required<StateAndOptionalOutput<T, O>>;
+export const definitelyUnusedRemoveOnceWeKnowCiWorks = true;
 
-export function mapWithState<T, I, O>(
-  fn: (state: T, input: I) => StateAndOutput<T, O>,
-  initialState: T
-): OperatorFunction<I, O> {
-  return pipe(
-    scan(({ state }: StateAndOptionalOutput<T, O>, input: I): StateAndOptionalOutput<T, O> => fn(state, input), {
-      state: initialState,
-    }),
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    map(({ output }) => output!)
-  );
-}
+export type StateAndOutput<T, O> = Required<StateAndOptionalOutput<T, O>>;
 
 export function mergeMapWithState<T, I, O>(
   fn: (state: T, input: I) => ObservableInput<StateAndOutput<T, O>>,
