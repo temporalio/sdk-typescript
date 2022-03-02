5d2097e7fced3a33c50867ce3b8e03aaf0a05524
Loren ☺️ <251288+lorensr@users.noreply.github.com>
2022-03-02 18:57:06 -0500

    fix: Re-export possibly-shared-use things in common (#509)

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -3,7 +3,10 @@
  *
  * @module
  */
+export { ActivityFunction, ActivityInterface, Headers, Next, RetryPolicy } from '@temporalio/internal-workflow-common';
 export * from '@temporalio/internal-workflow-common/lib/errors';
+export * from '@temporalio/internal-workflow-common/lib/interfaces';
+export * from '@temporalio/internal-workflow-common/lib/time';
 export * from './converter/data-converter';
 export * from './converter/payload-codec';
 export * from './converter/payload-converter';
