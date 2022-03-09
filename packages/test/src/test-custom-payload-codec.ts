e82e939065b8403ffa883841af1f4d42e2d04592
Loren 🤓 <lorensr@gmail.com>
2022-03-09 11:41:21 -0500

    Update tests

--- packages/test/src/test-custom-payload-codec.ts
+++ packages/test/src/test-custom-payload-codec.ts
@@ -1,5 +1,5 @@
 import { Connection, WorkflowClient } from '@temporalio/client';
-import { Payload, PayloadCodec } from '@temporalio/common';
+import { EncodedPayload, Payload, PayloadCodec } from '@temporalio/common';
 import { InjectedSinks, Worker } from '@temporalio/worker';
 import test from 'ava';
 import { v4 as uuid4 } from 'uuid';
@@ -9,11 +9,12 @@ import { defaultOptions } from './mock-native-worker';
 import { LogSinks, twoStrings, twoStringsActivity } from './workflows';
 
 class TestEncodeCodec implements PayloadCodec {
-  async encode(payloads: Payload[]): Promise<Payload[]> {
-    return payloads.map((payload) => {
-      payload.data = u8('"encoded"');
-      return payload;
-    });
+  async encode(payloads: Payload[]): Promise<EncodedPayload[]> {
+    return payloads.map((payload) => ({
+      ...payload,
+      data: u8('"encoded"'),
+      encoded: true,
+    }));
   }
 
   async decode(payloads: Payload[]): Promise<Payload[]> {
@@ -22,8 +23,8 @@ class TestEncodeCodec implements PayloadCodec {
 }
 
 class TestDecodeCodec implements PayloadCodec {
-  async encode(payloads: Payload[]): Promise<Payload[]> {
-    return payloads;
+  async encode(payloads: Payload[]): Promise<EncodedPayload[]> {
+    return payloads as EncodedPayload[];
   }
 
   async decode(payloads: Payload[]): Promise<Payload[]> {
--- packages/test/src/test-integration-codec.ts
+++ packages/test/src/test-integration-codec.ts
@@ -1,15 +1,14 @@
-import { Payload, PayloadCodec } from '@temporalio/common';
+import { EncodedPayload, Payload, PayloadCodec } from '@temporalio/common';
 import { RUN_INTEGRATION_TESTS } from './helpers';
 import { runIntegrationTests } from './integration-tests';
 
 class TestPayloadCodec implements PayloadCodec {
-  async encode(payloads: Payload[]): Promise<Payload[]> {
-    return payloads.map((payload) => {
-      if (payload.data) {
-        payload.data = payload.data.map((byte) => byte + 1);
-      }
-      return payload;
-    });
+  async encode(payloads: Payload[]): Promise<EncodedPayload[]> {
+    return payloads.map((payload) => ({
+      ...payload,
+      ...(payload.data && { data: payload.data.map((byte) => byte + 1) }),
+      encoded: true,
+    }));
   }
 
   async decode(payloads: Payload[]): Promise<Payload[]> {
