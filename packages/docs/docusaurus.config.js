8c2dd1f377f881762bc36b17e8dac8d402965dbd
Valeri Karpov <val@karpov.io>
2022-04-29 13:39:31 -0400

    make snipsync pull from samples-typescript
    Fix #544

--- packages/docs/docusaurus.config.js
+++ packages/docs/docusaurus.config.js
@@ -133,8 +133,12 @@ module.exports = {
             {
               origins: [
                 {
-                  files: ['../*/src/**/*.ts', '../create-project/samples/*.ts'],
+                  files: ['../*/src/**/*.ts', '../*/src/*.ts', '../create-project/samples/*.ts'],
                 },
+                {
+                  owner: 'temporalio',
+                  repo: 'samples-typescript'
+                }
               ],
               targets: ['docs'],
               features: {
