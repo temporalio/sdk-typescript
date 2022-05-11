8662ed25bd81da6a469255c7e67c8834ff54d203
Roey Berman <roey.berman@gmail.com>
2022-05-11 15:29:20 -0700

    feat!(worker): Restructure `TelemetryOptions`
    * Also support passing HTTP headers to an OpenTelemetry Collector

--- packages/worker/src/index.ts
+++ packages/worker/src/index.ts
@@ -8,12 +8,13 @@
  * @module
  */
 
+export { TelemetryOptions, Logger as TelemLogger, ForwardLogger, ConsoleLogger } from '@temporalio/core-bridge';
 export { NativeConnection as NativeConnection } from './connection';
 export { NativeConnectionOptions, RequiredNativeConnectionOptions, TLSConfig } from './connection-options';
 export * from './errors';
 export * from './interceptors';
 export * from './logger';
-export { History, Runtime, RuntimeOptions, TelemetryOptions } from './runtime';
+export { History, Runtime, RuntimeOptions } from './runtime';
 export * from './sinks';
 export { DataConverter, defaultPayloadConverter, errors, State, Worker } from './worker';
 export { CompiledWorkerOptions, WorkerOptions } from './worker-options';
