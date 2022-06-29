028f951a6e20d84a07dfa728fcbc1707dcf59db9
James Watkins-Harvey <jwatkins@progi-media.com>
2022-06-29 18:04:53 -0400

    fix(bundler): avoid 'package source-map-loader not found' in PNPM

--- packages/worker/src/workflow/bundler.ts
+++ packages/worker/src/workflow/bundler.ts
@@ -185,7 +185,7 @@ export class WorkflowCodeBundler {
           {
             test: /\.js$/,
             enforce: 'pre',
-            use: ['source-map-loader'],
+            use: [require.resolve('source-map-loader')],
           },
           {
             test: /\.ts$/,
