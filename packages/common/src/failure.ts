10986230799295098a782ddb485c2fa0b1518ea0
Loren ☺️ <251288+lorensr@users.noreply.github.com>
2022-07-22 13:24:08 -0400

    Update packages/common/src/failure.ts
    Co-authored-by: Roey Berman <roey@temporal.io>

--- packages/common/src/failure.ts
+++ packages/common/src/failure.ts
@@ -126,7 +126,7 @@ export class ApplicationFailure extends TemporalFailure {
    * By default, will be retryable (unless its `type` is included in {@link RetryPolicy.nonRetryableErrorTypes}).
    */
   public static create(options: ApplicationFailureOptions): ApplicationFailure {
-    const { message, type, nonRetryable = false, details, cause } = options ?? {};
+    const { message, type, nonRetryable = false, details, cause } = options;
     return new this(message, type, nonRetryable, details, cause);
   }
 
