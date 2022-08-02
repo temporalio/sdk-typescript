82eb8f6e6e5edf401c12bf12476c19d0ccd8d485
Nik B <bashkirov.office@gmail.com>
2022-08-02 14:51:20 -0700

    Made corrections according to the discussion

--- packages/test/src/integration-tests.ts
+++ packages/test/src/integration-tests.ts
@@ -1219,12 +1219,16 @@ export function runIntegrationTests(codec?: PayloadCodec): void {
     });
 
     const key: string = enhStacks[1].stacks[0].locations[0].filepath;
-    t.is(enhStacks[0].sdk.name, 'typescript');
-    t.is(enhStacks[0].stacks[0].locations.length, 3);
-    t.is(enhStacks[1].stacks[0].locations.length, 6);
-    console.log(enhStacks[1].sources[key][0].content.split('\n').length);
-    console.log(enhStacks[1].sources[key][0].content.split('\n'));
-    t.is(enhStacks[1].sources[key][0].content.split('\n').length, 50);
+    t.is(enhStacks[0].sdk.name, 'typescript', 'sdk name is not "typescript"');
+    t.is(enhStacks[0].stacks.length, 3, 'Number of stacks is different from expected');
+    t.is(enhStacks[0].stacks[0].locations.length, 1, 'Number of locations is different from expected');
+    t.is(enhStacks[1].stacks.length, 3, 'Number of stacks is different from expected');
+    t.is(enhStacks[1].stacks[0].locations.length, 2, 'Number of locations is different from expected');
+    t.is(
+      enhStacks[1].sources[key][0].content.split('\n').length,
+      50,
+      'Number of lines in stack trace is different from expected'
+    );
   });
 
   test('issue-731', async (t) => {
