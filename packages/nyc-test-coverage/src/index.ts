5ca2b8d848c02fad61d115b1277f5d33a6031841
Valeri Karpov <val@karpov.io>
2022-08-05 15:23:01 -0400

    address Roeys comments

--- packages/nyc-test-coverage/src/index.ts
+++ packages/nyc-test-coverage/src/index.ts
@@ -1,10 +1,8 @@
-import {
-  InjectedSinks,
-} from '@temporalio/worker';
+import { InjectedSinks } from '@temporalio/worker';
 import { CoverageSinks } from './sinks';
 import libCoverage from 'istanbul-lib-coverage';
 
-export function createCoverageSinks(): { sinks: InjectedSinks<CoverageSinks>, coverageMap: libCoverage.CoverageMap } {
+export function createCoverageSinks(): { sinks: InjectedSinks<CoverageSinks>; coverageMap: libCoverage.CoverageMap } {
   const coverageMap = libCoverage.createCoverageMap();
 
   return {
@@ -18,11 +16,12 @@ export function createCoverageSinks(): { sinks: InjectedSinks<CoverageSinks>, co
           callDuringReplay: false,
         },
       },
-    }
+    },
   };
-};
+}
 
-export function mergeWorkflowCoverage(coverageMap: any) {
+export function mergeWorkflowCoverage(coverageMap: libCoverage.CoverageMap): void {
+  /* eslint-disable @typescript-eslint/ban-ts-comment */
   // @ts-ignore
   coverageMap.merge(global.__coverage__);
   // @ts-ignore
--- packages/nyc-test-coverage/src/interceptors.ts
+++ packages/nyc-test-coverage/src/interceptors.ts
@@ -1,53 +1,25 @@
-import {
-  proxySinks,
-  SignalInput,
-  WorkflowExecuteInput,
-  WorkflowInterceptorsFactory,
-  WorkflowInboundCallsInterceptor,
-  QueryInput
-} from '@temporalio/workflow';
+import { proxySinks, WorkflowInterceptors } from '@temporalio/workflow';
 import { CoverageSinks } from './sinks';
 
 const { coverage } = proxySinks<CoverageSinks>();
 
-class CoverageInterceptor implements WorkflowInboundCallsInterceptor {
-  public async handleQuery(input: QueryInput, next: any): Promise<unknown> {
-    const ret = await next(input);
-
-    // @ts-ignore
-    const globalCoverage = global.__coverage__;
-    coverage.merge(JSON.parse(JSON.stringify(globalCoverage)));
-    clearCoverage(globalCoverage);
-
-    return ret;
-  }
-
-  public async handleSignal(input: SignalInput, next: any): Promise<void> {
-    const ret = await next(input);
-
-    // @ts-ignore
-    const globalCoverage = global.__coverage__;
-    coverage.merge(JSON.parse(JSON.stringify(globalCoverage)));
-    clearCoverage(globalCoverage);
-
-    return ret;
-  }
-
-  public async execute(input: WorkflowExecuteInput, next: any): Promise<unknown> {
-    const ret = await next(input);
-
-    // @ts-ignore
-    const globalCoverage = global.__coverage__;
-    coverage.merge(JSON.parse(JSON.stringify(globalCoverage)));
-    clearCoverage(globalCoverage);
-
-    return ret;
-  }
-}
-
 // Export the interceptors
-export const interceptors: WorkflowInterceptorsFactory = () => ({
-  inbound: [new CoverageInterceptor()],
+export const interceptors = (): WorkflowInterceptors => ({
+  internals: [
+    {
+      concludeActivation(input, next) {
+        /* eslint-disable @typescript-eslint/ban-ts-comment */
+        // @ts-ignore
+        const globalCoverage = global.__coverage__;
+        coverage.merge(JSON.parse(JSON.stringify(globalCoverage)));
+        clearCoverage(globalCoverage);
+
+        return next(input);
+      },
+    },
+  ],
+  inbound: [],
+  outbound: [],
 });
 
 function clearCoverage(coverage: any): void {
