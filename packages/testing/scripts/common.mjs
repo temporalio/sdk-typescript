5d5e74d011223f0196b421f77488dfb2a5e59896
Roey Berman <roey.berman@gmail.com>
2022-09-14 13:36:57 -0700

    Some cleanup

--- packages/testing/scripts/common.mjs
+++
@@ -1,5 +0,0 @@
-import { URL, fileURLToPath } from 'node:url';
-import os from 'node:os';
-
-const ext = os.platform() === 'win32' ? '.exe' : '';
-export const outputPath = fileURLToPath(new URL(`../test-server${ext}`, import.meta.url));
