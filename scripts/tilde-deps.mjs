f237ea66f25c2605506f57e3ade866ee4c6ac637
Roey Berman <roey.berman@gmail.com>
2022-11-03 21:39:49 -0700

    chore: Minor fixes to contrib guide

---
+++ scripts/tilde-deps.mjs
@@ -0,0 +1,22 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import { URL, fileURLToPath } from 'node:url';
+
+const rootPath = fileURLToPath(new URL('..', import.meta.url));
+const packagesPath = path.join(rootPath, 'packages');
+const lernaJsonPath = path.join(rootPath, 'lerna.json');
+
+const { version } = JSON.parse(await fs.readFile(lernaJsonPath, 'utf8'));
+
+const packages = await fs.readdir(packagesPath);
+for (const dir of packages) {
+  const packageJsonPath = path.join(packagesPath, dir, 'package.json');
+  const packageJson = JSON.parse(await fs.readFile(packageJsonPath, 'utf8'));
+  for (const dep of Object.keys(packageJson.dependencies)) {
+    if (dep.startsWith('@temporalio/')) {
+      packageJson.dependencies[dep] = `~${version}`;
+    }
+  }
+  const replacedContent = JSON.stringify(packageJson, null, 2);
+  await fs.writeFile(packageJsonPath, replacedContent + '\n');
+}
