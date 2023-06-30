8d3a3f1770a0962b9f92367f31795d6ce9f66994
Roey Berman <roey.berman@gmail.com>
2023-06-30 08:19:05 -0700

    Make log attributes optional for defaultWorkerLogger sink

--- packages/workflow/src/workflow.ts
+++ packages/workflow/src/workflow.ts
@@ -1308,11 +1308,11 @@ export const log: LoggerSinks['defaultWorkerLogger'] = Object.fromEntries(
   (['trace', 'debug', 'info', 'warn', 'error'] as Array<keyof LoggerSinks['defaultWorkerLogger']>).map((level) => {
     return [
       level,
-      (message: string, attrs: Record<string, unknown>) => {
-        assertInWorkflowContext('Workflow.log(...) may only be used from a Workflow Execution.)');
+      (message: string, attrs?: Record<string, unknown>) => {
+        const activator = assertInWorkflowContext('Workflow.log(...) may only be used from a Workflow Execution.');
         return loggerSinks.defaultWorkerLogger[level](message, {
           // Inject the call time in nanosecond resolution as expected by the worker logger.
-          [LogTimestamp]: getActivator().getTimeOfDay(),
+          [LogTimestamp]: activator.getTimeOfDay(),
           ...attrs,
         });
       },
