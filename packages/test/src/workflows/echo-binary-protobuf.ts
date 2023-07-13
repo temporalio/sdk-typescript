4bc5c3e22262860c7bafd9482f7fb32b92c7ded3
Antonio Lain <antonio.lain@temporal.io>
2023-07-12 23:01:28 -0700

    Fix format

--- packages/test/src/workflows/echo-binary-protobuf.ts
+++ packages/test/src/workflows/echo-binary-protobuf.ts
@@ -1,5 +1,6 @@
-import type { BinaryMessage } from '../../protos/root';
 import { ApplicationFailure } from '@temporalio/common';
+import type { BinaryMessage } from '../../protos/root';
+
 export async function echoBinaryProtobuf(input: BinaryMessage): Promise<BinaryMessage> {
   if (input.data instanceof Uint8Array) {
     return input;
