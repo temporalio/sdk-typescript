533b1552dbb55841206b2e5aac8aab544da9b420
James Watkins-Harvey <james.watkinsharvey@temporal.io>
2023-09-11 22:42:33 -0400

    Add unit test

--- packages/test/src/test-interface-type-safety.ts
+++ packages/test/src/test-interface-type-safety.ts
@@ -1,5 +1,6 @@
 import test from 'ava';
-import { defineSignal, defineQuery } from '@temporalio/workflow';
+import { defineSignal, defineQuery, ExternalWorkflowHandle, ChildWorkflowHandle, Workflow } from '@temporalio/workflow';
+import { WorkflowHandle } from '@temporalio/client';
 
 test('SignalDefinition Name type safety', (t) => {
   // @ts-expect-error Assert expect a type error when generic and concrete names do not match
@@ -51,5 +52,15 @@ test('QueryDefinition Args and Ret type safety', (t) => {
   type ArgTypeAssertion = typeof argVariantB extends typeof argVariantA ? 'intermixable' : 'not-intermixable';
 
   const _argAssertion: ArgTypeAssertion = 'not-intermixable';
+  t.pass();
+});
+
+test('Can call signal on any WorkflowHandle', async (t) => {
+  // This function definition is an assertion by itself. TSC will throw a compile time error if
+  // the signature of the signal function is not compatible across all WorkflowHandle variants.
+  function _assertion<T extends Workflow>(handle: WorkflowHandle<T> | ChildWorkflowHandle<T> | ExternalWorkflowHandle) {
+    handle.signal(defineSignal('signal'));
+  }
+
   t.pass();
 });
