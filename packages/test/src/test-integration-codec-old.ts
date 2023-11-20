f73bf7f777351c7ceffc628b62416d0a1239279f
Dan Davison <dan.davison@temporal.io>
2023-11-20 17:33:26 -0500

    Refactor integration tests

--- packages/test/src/test-integration-codec-old.ts
+++ packages/test/src/test-integration-codec-old.ts
@@ -1,5 +1,5 @@
 import { RUN_INTEGRATION_TESTS, ByteSkewerPayloadCodec } from './helpers';
-import { runIntegrationTests } from './integration-tests';
+import { runIntegrationTests } from './integration-tests-old';
 
 if (RUN_INTEGRATION_TESTS) {
   runIntegrationTests(new ByteSkewerPayloadCodec());
--- packages/test/src/test-integration-old.ts
+++ packages/test/src/test-integration-old.ts
@@ -1,5 +1,5 @@
 import { RUN_INTEGRATION_TESTS } from './helpers';
-import { runIntegrationTests } from './integration-tests';
+import { runIntegrationTests } from './integration-tests-old';
 
 if (RUN_INTEGRATION_TESTS) {
   runIntegrationTests();
