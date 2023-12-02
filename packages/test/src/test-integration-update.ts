bd6ebce414bafc3233fbdc3837d74857ea7cbd8c
Dan Davison <dan.davison@temporal.io>
2023-12-02 15:52:40 -0500

    Unite integration tests in single file to share server process

--- packages/test/src/test-integration-update.ts
+++ packages/test/src/test-integration-update.ts
@@ -348,3 +348,43 @@ test('Update handler is called at same point during first execution and replay',
     t.is(handlerWasExecutedEarly, true);
   });
 });
+
+/* Example from WorkflowHandle docstring */
+
+// @@@SNIPSTART typescript-workflow-update-signal-query-example
+export const incrementSignal = wf.defineSignal<[number]>('increment');
+export const getValueQuery = wf.defineQuery<number>('getValue');
+export const incrementAndGetValueUpdate = wf.defineUpdate<number, [number]>('incrementAndGetValue');
+
+export async function counterWorkflow(initialValue: number): Promise<void> {
+  let count = initialValue;
+  wf.setHandler(incrementSignal, (arg: number) => {
+    count += arg;
+  });
+  wf.setHandler(getValueQuery, () => count);
+  wf.setHandler(incrementAndGetValueUpdate, (arg: number): number => {
+    count += arg;
+    return count;
+  });
+  await wf.condition(() => false);
+}
+// @@@SNIPEND
+
+/* Example from WorkflowHandle docstring */
+test('Update/Signal/Query example in WorkflowHandle docstrings works', async (t) => {
+  const { createWorker, startWorkflow, assertWorkflowFailedError } = helpers(t);
+  const worker = await createWorker();
+  await worker.runUntil(async () => {
+    const wfHandle = await startWorkflow(counterWorkflow, { args: [2] });
+    await wfHandle.signal(incrementSignal, 2);
+    const queryResult = await wfHandle.query(getValueQuery);
+    t.is(queryResult, 4);
+    const updateResult = await wfHandle.executeUpdate(incrementAndGetValueUpdate, { args: [2] });
+    t.is(updateResult, 6);
+    const secondUpdateHandle = await wfHandle.startUpdate(incrementAndGetValueUpdate, { args: [2] });
+    const secondUpdateResult = await secondUpdateHandle.result();
+    t.is(secondUpdateResult, 8);
+    await wfHandle.cancel();
+    await assertWorkflowFailedError(wfHandle.result(), wf.CancelledFailure);
+  });
+});
