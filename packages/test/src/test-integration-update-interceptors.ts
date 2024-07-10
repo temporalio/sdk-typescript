790bcc4c11ec3ee38a2ebf91617017e689e0175c
James Watkins-Harvey <james.watkinsharvey@temporal.io>
2024-07-10 17:09:07 -0400

    Force CLI v0.13.2

--- packages/test/src/test-integration-update-interceptors.ts
+++ packages/test/src/test-integration-update-interceptors.ts
@@ -49,7 +49,7 @@ export const interceptors = (): WorkflowInterceptors => ({
   inbound: [new UpdateInboundCallsInterceptor()],
 });
 
-test.skip('Update client and inbound interceptors work for executeUpdate', async (t) => {
+test('Update client and inbound interceptors work for executeUpdate', async (t) => {
   const { createWorker, startWorkflow } = helpers(t);
   const worker = await createWorker();
   await worker.runUntil(async () => {
@@ -60,7 +60,7 @@ test.skip('Update client and inbound interceptors work for executeUpdate', async
   });
 });
 
-test.skip('Update client and inbound interceptors work for startUpdate', async (t) => {
+test('Update client and inbound interceptors work for startUpdate', async (t) => {
   const { createWorker, startWorkflow } = helpers(t);
   const worker = await createWorker();
   await worker.runUntil(async () => {
@@ -75,7 +75,7 @@ test.skip('Update client and inbound interceptors work for startUpdate', async (
   });
 });
 
-test.skip('Update validation interceptor works', async (t) => {
+test('Update validation interceptor works', async (t) => {
   const { createWorker, startWorkflow, assertWorkflowUpdateFailed } = helpers(t);
   const worker = await createWorker();
   await worker.runUntil(async () => {
@@ -95,7 +95,7 @@ export async function workflowWithUpdateWithoutValidator(): Promise<void> {
   await wf.condition(() => false); // Ensure the update is handled if it is dispatched in a second WFT.
 }
 
-test.skip('Update validation interceptors are not run when no validator', async (t) => {
+test('Update validation interceptors are not run when no validator', async (t) => {
   const { createWorker, startWorkflow } = helpers(t);
   const worker = await createWorker();
   await worker.runUntil(async () => {
