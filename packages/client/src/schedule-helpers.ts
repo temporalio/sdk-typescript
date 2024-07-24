8732c67735f11c91238dbd75690b8c294d514d6f
James Watkins-Harvey <mjameswh@users.noreply.github.com>
2024-07-24 13:03:21 -0400

    fix(worker): Don't fail Worker on Activity Task decode failures (#1473)
    (cherry picked from commit 4dd83c15c31419d276cd53c94f1ec9853ee9b686)

--- packages/client/src/schedule-helpers.ts
+++ packages/client/src/schedule-helpers.ts
@@ -23,7 +23,7 @@ import {
   optionalDateToTs,
   optionalTsToDate,
   optionalTsToMs,
-  tsToDate,
+  requiredTsToDate,
 } from '@temporalio/common/lib/time';
 import {
   CalendarSpec,
@@ -405,10 +405,8 @@ export function decodeScheduleRecentActions(
       } else throw new TypeError('Unsupported schedule action');
 
       return {
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        scheduledAt: tsToDate(executionResult.scheduleTime!),
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        takenAt: tsToDate(executionResult.actualTime!),
+        scheduledAt: requiredTsToDate(executionResult.scheduleTime, 'scheduleTime'),
+        takenAt: requiredTsToDate(executionResult.actualTime, 'actualTime'),
         action,
       };
     }
