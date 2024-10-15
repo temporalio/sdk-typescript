486408ce9ea8fea50ca248044a25ebc601b89ad8
James Watkins-Harvey <james.watkinsharvey@temporal.io>
2024-10-15 18:03:04 -0400

    fillMemoryWorkflow: use eval instead of IIFE to delay JIT compilation of code

--- packages/test/src/workflows/fill-memory.ts
+++ packages/test/src/workflows/fill-memory.ts
@@ -1,16 +1,19 @@
-export async function fillMemory(): Promise<void> {
+export async function fillMemory(): Promise<number> {
   // It looks like JIT compilation of the following code affects the way the out of memory error
   // will get reported. That is, once the code has been optimized, the out of memory condition is
   // very likely to trigger a low level (native-like) error, which may terminate the whole process,
   // rather than just the Worker Thread.
   //
-  // By wrapping this code in an IIFE, we delay JIT compilation of that code, thus reducing the
-  // probability of test flakes.
-  (() => {
-    const accumulator = [];
-    // Allocate 4GB of memory
+  // Wrapping code in an eval block delays JIT compilation, thus reducing the probability of flakes.
+
+  // Allocate 32GB of memory
+  // V8 internally stores numbers as 8 bytes, so each pass allocates 16MB
+  const accumulator = [];
+  eval(`
     for (let i = 0; i < 2048; i++) {
       accumulator.push(new Array(1024 * 1024 * 2).fill(i));
     }
-  })();
+  `);
+
+  return accumulator.length;
 }
