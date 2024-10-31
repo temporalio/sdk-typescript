aa59d72351d41331f9f3ffa796f948a3068b1c76
James Watkins-Harvey <mjameswh@users.noreply.github.com>
2024-10-31 09:42:03 -0400

    feat(client): Add utility functions isGrpcDeadlineError and isGrpcCancelledError (#1548)

--- packages/cloud/src/cloud-operations-client.ts
+++ packages/cloud/src/cloud-operations-client.ts
@@ -59,7 +59,7 @@ export class CloudOperationsClient {
    *
    * The deadline is a point in time after which any pending gRPC request will be considered as failed;
    * this will locally result in the request call throwing a {@link grpc.ServiceError|ServiceError}
-   * with code {@link grpc.status.DEADLINE_EXCEEDED|DEADLINE_EXCEEDED}.
+   * with code {@link grpc.status.DEADLINE_EXCEEDED|DEADLINE_EXCEEDED}; see {@link isGrpcDeadlineError}.
    *
    * It is stronly recommended to explicitly set deadlines. If no deadline is set, then it is
    * possible for the client to end up waiting forever for a response.
@@ -80,7 +80,7 @@ export class CloudOperationsClient {
   /**
    * Set an {@link AbortSignal} that, when aborted, cancels any ongoing service requests executed in
    * `fn`'s scope. This will locally result in the request call throwing a {@link grpc.ServiceError|ServiceError}
-   * with code {@link grpc.status.CANCELLED|CANCELLED}.
+   * with code {@link grpc.status.CANCELLED|CANCELLED}; see {@link isGrpcCancelledError}.
    *
    * This method is only a convenience wrapper around {@link CloudOperationsConnection.withAbortSignal}.
    *
@@ -468,7 +468,7 @@ export class CloudOperationsConnection {
    *
    * The deadline is a point in time after which any pending gRPC request will be considered as failed;
    * this will locally result in the request call throwing a {@link grpc.ServiceError|ServiceError}
-   * with code {@link grpc.status.DEADLINE_EXCEEDED|DEADLINE_EXCEEDED}.
+   * with code {@link grpc.status.DEADLINE_EXCEEDED|DEADLINE_EXCEEDED}; see {@link isGrpcDeadlineError}.
    *
    * It is stronly recommended to explicitly set deadlines. If no deadline is set, then it is
    * possible for the client to end up waiting forever for a response.
