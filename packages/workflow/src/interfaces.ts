4155dad08af8e93685eed3e41c09495eb7d79de3
Thomas Hardy <thomas.hardy@temporal.io>
2025-03-10 10:25:53 -0700

    add default query handler (#1639)

--- packages/workflow/src/interfaces.ts
+++ packages/workflow/src/interfaces.ts
@@ -539,6 +539,11 @@ export type Handler<
 export type DefaultSignalHandler = (signalName: string, ...args: unknown[]) => void | Promise<void>;
 
 /**
+ * A handler function accepting query calls for non-registered query names.
+ */
+export type DefaultQueryHandler = (queryName: string, ...args: unknown[]) => unknown;
+
+/**
  * A validation function capable of accepting the arguments for a given UpdateDefinition.
  */
 export type UpdateValidator<Args extends any[]> = (...args: Args) => void;
