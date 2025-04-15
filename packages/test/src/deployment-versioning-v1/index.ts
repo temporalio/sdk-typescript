51332f9cccf4b4901455565d6ede30fa4ecdb714
Spencer Judge <spencer@temporal.io>
2025-04-15 16:13:43 -0700

    Dynamic workflow support

--- packages/test/src/deployment-versioning-v1/index.ts
+++ packages/test/src/deployment-versioning-v1/index.ts
@@ -9,3 +9,9 @@ export async function deploymentVersioning(): Promise<string> {
   await condition(() => doFinish);
   return 'version-v1';
 }
+
+// Dynamic/default workflow handler
+export default defineWorkflowWithOptions({ versioningBehavior: 'pinned' }, _default);
+async function _default(): Promise<string> {
+  return 'dynamic';
+}
