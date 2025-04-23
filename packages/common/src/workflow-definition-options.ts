04368aee6d80e1cb1574ad488aba240d339a9eab
Spencer Judge <spencer@temporal.io>
2025-04-23 16:46:44 -0700

    Options getter function

--- packages/common/src/workflow-definition-options.ts
+++ packages/common/src/workflow-definition-options.ts
@@ -8,11 +8,12 @@ export interface WorkflowDefinitionOptions {
 }
 
 type AsyncFunction<Args extends any[], ReturnType> = (...args: Args) => Promise<ReturnType>;
+export type WorkflowDefinitionOptionsOrGetter = WorkflowDefinitionOptions | (() => WorkflowDefinitionOptions);
 
 /**
  * A workflow function that has been defined with options from {@link WorkflowDefinitionOptions}.
  */
 export interface WorkflowFunctionWithOptions<Args extends any[], ReturnType> extends AsyncFunction<Args, ReturnType> {
   __temporal_is_workflow_function_with_options: true;
-  options: WorkflowDefinitionOptions;
+  options: WorkflowDefinitionOptionsOrGetter;
 }
