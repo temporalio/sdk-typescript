543ca388d3f66dfbc2315a617531171fc807acc6
James Watkins-Harvey <james.watkinsharvey@temporal.io>
2025-05-11 23:40:17 -0400

    fix(client): Properly set temporal-namespace header on gRPC requests (#1712)
    (backported from commit ea28266ae12280df4cd8da6a3ff168dd6b47e1f7)

--- packages/test/src/test-native-connection-headers.ts
+++ packages/test/src/test-native-connection-headers.ts
@@ -1,5 +1,6 @@
 import util from 'node:util';
 import path from 'node:path';
+import assert from 'node:assert';
 import test from 'ava';
 import { Subject, firstValueFrom, skip } from 'rxjs';
 import * as grpc from '@grpc/grpc-js';
@@ -7,6 +8,19 @@ import * as protoLoader from '@grpc/proto-loader';
 import { NativeConnection } from '@temporalio/worker';
 import { temporal } from '@temporalio/proto';
 import { Worker } from './helpers';
+
+const workflowServicePackageDefinition = protoLoader.loadSync(
+  path.resolve(
+    __dirname,
+    '../../core-bridge/sdk-core/sdk-core-protos/protos/api_upstream/temporal/api/workflowservice/v1/service.proto'
+  ),
+  { includeDirs: [path.resolve(__dirname, '../../core-bridge/sdk-core/sdk-core-protos/protos/api_upstream')] }
+);
+const workflowServiceProtoDescriptor = grpc.loadPackageDefinition(workflowServicePackageDefinition) as any;
+
+async function bindLocalhost(server: grpc.Server): Promise<number> {
+  return await util.promisify(server.bindAsync.bind(server))('127.0.0.1:0', grpc.ServerCredentials.createInsecure());
+}
 
 test('NativeConnection passes headers provided in options', async (t) => {
   const packageDefinition = protoLoader.loadSync(
@@ -88,4 +102,45 @@ test('NativeConnection passes headers provided in options', async (t) => {
     },
   });
   await Promise.all([firstValueFrom(newValuesSubject.pipe(skip(1))).then(() => worker.shutdown()), worker.run()]);
+});
+
+test('apiKey sets temporal-namespace header appropriately', async (t) => {
+  let getSystemInfoHeaders: grpc.Metadata = new grpc.Metadata();
+  let startWorkflowExecutionHeaders: grpc.Metadata = new grpc.Metadata();
+
+  const server = new grpc.Server();
+  server.addService(workflowServiceProtoDescriptor.temporal.api.workflowservice.v1.WorkflowService.service, {
+    getSystemInfo(
+      call: grpc.ServerUnaryCall<
+        temporal.api.workflowservice.v1.IGetSystemInfoRequest,
+        temporal.api.workflowservice.v1.IGetSystemInfoResponse
+      >,
+      callback: grpc.sendUnaryData<temporal.api.workflowservice.v1.IGetSystemInfoResponse>
+    ) {
+      getSystemInfoHeaders = call.metadata.clone();
+      callback(null, {});
+    },
+    startWorkflowExecution(call: grpc.ServerUnaryCall<any, any>, callback: grpc.sendUnaryData<any>) {
+      startWorkflowExecutionHeaders = call.metadata.clone();
+      callback(null, {});
+    },
+  });
+  const port = await bindLocalhost(server);
+  const conn = await NativeConnection.connect({
+    address: `127.0.0.1:${port}`,
+    metadata: { staticKey: 'set' },
+    apiKey: 'test-token',
+  });
+
+  await conn.workflowService.startWorkflowExecution({ namespace: 'test-namespace' });
+
+  assert(getSystemInfoHeaders !== undefined);
+  t.deepEqual(getSystemInfoHeaders.get('temporal-namespace'), []);
+  t.deepEqual(getSystemInfoHeaders.get('authorization'), ['Bearer test-token']);
+  t.deepEqual(getSystemInfoHeaders.get('staticKey'), ['set']);
+
+  assert(startWorkflowExecutionHeaders);
+  t.deepEqual(startWorkflowExecutionHeaders.get('temporal-namespace'), ['test-namespace']);
+  t.deepEqual(startWorkflowExecutionHeaders.get('authorization'), ['Bearer test-token']);
+  t.deepEqual(startWorkflowExecutionHeaders.get('staticKey'), ['set']);
 });
