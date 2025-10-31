8d2cdf515f120cd9c85334cea3be9bf08c9241f8
Chris Olszewski <chris.olszewski@temporal.io>
2025-10-31 12:20:03 -0400

    silence require warnings

--- packages/interceptors-opentelemetry/src/workflow/workflow-module-loader.ts
+++ packages/interceptors-opentelemetry/src/workflow/workflow-module-loader.ts
@@ -40,7 +40,9 @@ export function getWorkflowModule(): typeof WorkflowModule {
  * Will throw if `@temporalio/workflow` is not available
  */
 export function hasSdkFlag(flag: keyof SdkFlags): boolean {
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
   const { SdkFlags } = require('@temporalio/workflow/lib/flags');
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
   const { getActivator } = require('@temporalio/workflow/lib/global-attributes');
 
   return getActivator().hasFlag(SdkFlags[flag]);
