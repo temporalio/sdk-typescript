15eda53de35a64cca3d0b01bef1f64abed96781f
Chris Olszewski <chris.olszewski@temporal.io>
2026-01-02 13:58:34 -0500

    chore: add failing test for GH 1866
    squash

---
+++ packages/test/src/workflows/reusable-vm-disposal-bug.ts
@@ -0,0 +1,20 @@
+import { condition } from '@temporalio/workflow';
+
+/**
+ * Workflow for reproducing the GH #1866
+ *
+ * When the bug is present:
+ * 1. First condition times out
+ * 2. finally block calls CancellationScope.current().cancel()
+ * 3. With disabled storage, current() returns rootScope
+ * 4. rootScope.cancel() is called, failing the workflow with "Workflow cancelled"
+ *
+ * Actual failure happens on the second `condition` where when creating the new
+ * cancellation scope, we see that the parent/root scope is already canceled.
+ */
+export async function conditionWithTimeoutAfterDisposal(): Promise<string> {
+  const alwaysFalse = false;
+  await condition(() => alwaysFalse, '500ms');
+  await condition(() => alwaysFalse, '500ms');
+  return 'done';
+}
