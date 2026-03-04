ddddc0729d348feffc004ae2c24957f07bbb4094
Quinn Klassen <klassenq@gmail.com>
2026-03-04 13:36:49 -0800

    Fix another test

--- packages/test/src/test-nexus-handler.ts
+++ packages/test/src/test-nexus-handler.ts
@@ -409,9 +409,7 @@ test('start Operation Handler errors', async (t) => {
       // Exact error message varies between Node versions and runtimes.
       t.regex(
         message,
-        isBun
-          ? /Payload converter failed to decode Nexus operation input: SyntaxError: JSON Parse error:/
-          : /Payload converter failed to decode Nexus operation input: SyntaxError: Unexpected token .* JSON/
+        isBun ? /JSON Parse error:/ : /Unexpected token .* JSON/
       );
     }
   });
