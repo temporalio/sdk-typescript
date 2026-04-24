edb9d785e773a974ab59020774982b9fbe042896
Chris Olszewski <chris.olszewski@temporal.io>
2026-04-24 16:40:30 -0400

    update comments

--- packages/test/src/run-activation-perf-tests.ts
+++ packages/test/src/run-activation-perf-tests.ts
@@ -104,7 +104,7 @@ if (!wf.inWorkflowContext()) {
     async function activate(workflow: VMWorkflow, activation: coresdk.workflow_activation.IWorkflowActivation) {
       // Core guarantees the following jobs ordering:
       //   initWf -> patches -> update random seed -> signals+update -> others -> Resolve LA
-      // reference: github.com/temporalio/sdk-core/blob/a8150d5c7c3fc1bfd5a941fd315abff1556cd9dc/core/src/worker/workflow/mod.rs#L1363-L1378
+      // reference: github.com/temporalio/sdk-rust/blob/a8150d5c7c3fc1bfd5a941fd315abff1556cd9dc/core/src/worker/workflow/mod.rs#L1363-L1378
       // Tests are likely to fail if we artifically make an activation that does not follow that order
       const jobs: coresdk.workflow_activation.IWorkflowActivationJob[] = activation.jobs ?? [];
       function getPriority(job: coresdk.workflow_activation.IWorkflowActivationJob) {
