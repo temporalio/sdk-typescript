bad6ef0ba65922bdde98ff0143e21ed58e0930ad
Thomas Hardy <thomas.hardy@temporal.io>
2026-06-11 09:31:40 -0400

    format

--- contrib/interceptors-opentelemetry/src/__tests__/test-otel.ts
+++ contrib/interceptors-opentelemetry/src/__tests__/test-otel.ts
@@ -38,7 +38,7 @@ import {
 import type * as workflowImportStub from '../workflow/workflow-imports';
 import type * as workflowImportImpl from '../workflow/workflow-imports-impl';
 import { instrument, instrumentSync, NEXUS_SERVICE_ATTR_KEY, NEXUS_OPERATION_ATTR_KEY } from '../instrumentation';
-import type { OpenTelemetryNexusInboundInterceptor } from '../worker'
+import type { OpenTelemetryNexusInboundInterceptor } from '../worker';
 import {
   makeWorkflowExporter,
   OpenTelemetryActivityInboundInterceptor,
