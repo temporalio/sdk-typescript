f9ba3b1c61aee114f5720d2296d2f4ac8119107d
Chris Constable <chris.constable@temporal.io>
2026-06-18 12:18:19 -0400

    feat(extstore): add @google-cloud/store gcs driver package.

---
+++ contrib/external-storage-gcs-google-sdk/src/index.ts
@@ -0,0 +1,4 @@
+/**
+ * @experimental The External Storage GCS driver is an experimental feature and may be subject to change.
+ */
+export { GoogleCloudGcsStorageDriverClient } from './google-cloud-client';
