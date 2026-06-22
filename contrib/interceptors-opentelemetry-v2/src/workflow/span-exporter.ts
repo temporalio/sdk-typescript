17abe6d478db81f6b8d707e58a10fa66bb386fc8
Chris Olszewski <chris.olszewski@temporal.io>
2026-06-22 12:15:23 -0400

    chore(otel-v2): copy opentelemetry interceptor package

---
+++ contrib/interceptors-opentelemetry-v2/src/workflow/span-exporter.ts
@@ -0,0 +1,50 @@
+import type * as tracing from '@opentelemetry/sdk-trace-base';
+import type { ExportResult } from '@opentelemetry/core';
+import { ExportResultCode } from '@opentelemetry/core';
+import type { OpenTelemetrySinks, SerializableSpan, SerializableSpanContext } from './definitions';
+import { proxySinks } from './workflow-imports';
+
+export class SpanExporter implements tracing.SpanExporter {
+  private exporter?: OpenTelemetrySinks['exporter'];
+
+  public export(spans: tracing.ReadableSpan[], resultCallback: (result: ExportResult) => void): void {
+    if (!this.exporter) {
+      this.exporter = proxySinks<OpenTelemetrySinks>().exporter;
+    }
+    this.exporter.export(spans.map((span) => this.makeSerializable(span)));
+    resultCallback({ code: ExportResultCode.SUCCESS });
+  }
+
+  public async shutdown(): Promise<void> {
+    // Nothing to shut down
+  }
+
+  public makeSerializable(span: tracing.ReadableSpan): SerializableSpan {
+    const { traceState, ...restSpanContext } = span.spanContext();
+    // Serialize traceState to a string because TraceState objects lose their
+    // prototype methods when crossing the V8 isolate boundary.
+    // See: https://github.com/temporalio/sdk-typescript/issues/1738
+    const serializableSpanContext: SerializableSpanContext = {
+      traceState: traceState?.serialize(),
+      ...restSpanContext,
+    };
+    return {
+      name: span.name,
+      kind: span.kind,
+      spanContext: serializableSpanContext,
+      parentSpanId: span.parentSpanId,
+      startTime: span.startTime,
+      endTime: span.endTime,
+      status: span.status,
+      attributes: span.attributes,
+      links: span.links,
+      events: span.events,
+      duration: span.duration,
+      ended: span.ended,
+      droppedAttributesCount: span.droppedAttributesCount,
+      droppedEventsCount: span.droppedEventsCount,
+      droppedLinksCount: span.droppedLinksCount,
+      instrumentationLibrary: span.instrumentationLibrary,
+    };
+  }
+}
