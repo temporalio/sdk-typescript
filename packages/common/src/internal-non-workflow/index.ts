e605020d4fcf8cfce217ef464bd60f6e34e76587
Chris Constable <chris.constable@temporal.io>
2026-06-24 14:41:42 -0400

    feat(extstore): add external storage runner core logic

--- packages/common/src/internal-non-workflow/index.ts
+++ packages/common/src/internal-non-workflow/index.ts
@@ -7,6 +7,7 @@ export * from './codec-helpers';
 export * from './codec-types';
 export * from './data-converter-helpers';
 export * from './extstore-helpers';
+export * from './external-storage-runner';
 export * from './parse-host-uri';
 export * from './proxy-config';
 export * from './tls-config';
