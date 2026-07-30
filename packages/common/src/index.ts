3ce87b1bc70f913909f9b7a530aaf96bd6ab56b1
Thomas Hardy <thomas.hardy@temporal.io>
2026-07-30 10:05:53 -0400

    fix(common): use type-only TypeInfo exports

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -31,7 +31,7 @@ export * from './priority';
 export * from './metrics';
 export * from './retry-policy';
 export type { Timestamp, Duration, StringValue } from './time';
-export { TransferTypeConverter, TypeInfo } from './type-info';
+export type { TransferTypeConverter, TypeInfo } from './type-info';
 export * from './worker-deployments';
 export * from './workflow-definition-options';
 export * from './workflow-handle';
