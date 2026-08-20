951a24a4ad4eae3edeb120a0f6a0ec391877d324
Chris Constable <chris.constable@temporal.io>
2026-08-20 13:11:10 -0400

    refactor(codec): use new payload visitor to decode workflow activations

--- packages/worker/src/workflow-codec-runner.ts
+++ packages/worker/src/workflow-codec-runner.ts
@@ -6,20 +6,17 @@ import type {
 } from '@temporalio/common';
 import type { Decoded, Encoded } from '@temporalio/common/lib/internal-non-workflow';
 import {
+  decode,
   encode,
-  decodeOptional,
-  decodeOptionalFailure,
-  decodeOptionalMap,
-  decodeOptionalSingle,
-  noopDecodeMap,
   visit,
+  walkWorkflowActivation,
   walkWorkflowActivationCompletion,
 } from '@temporalio/common/lib/internal-non-workflow';
 import { limit } from '@temporalio/common/lib/concurrency/limit';
 import { coresdk } from '@temporalio/proto';
 
 /**
- * Maximum number of concurrent codec encodes for workflow completions. Previously,
+ * Maximum number of concurrent codec calls per activation or completion. Previously,
  * this was unbounded.
  */
 const MAX_CONCURRENT_CODEC_OPERATIONS = 20;
@@ -100,259 +97,50 @@ export class WorkflowCodecRunner {
   public async decodeActivation<T extends coresdk.workflow_activation.IWorkflowActivation>(
     activation: T
   ): Promise<Decoded<T>> {
-    return coresdk.workflow_activation.WorkflowActivation.fromObject(<
-      Decoded<coresdk.workflow_activation.IWorkflowActivation>
-    >{
-      ...activation,
-      jobs: activation.jobs
-        ? await Promise.all(
-            activation.jobs.map(async (job) => {
-              const resolveActivityContext = job.resolveActivity
-                ? this.consumeContext(this.pendingCompletionContexts.activity, job.resolveActivity.seq)
-                : undefined;
-              const resolveChildWorkflowExecutionContext = job.resolveChildWorkflowExecution
-                ? this.consumeContext(
-                    this.pendingCompletionContexts.childWorkflowComplete,
-                    job.resolveChildWorkflowExecution.seq
-                  )
-                : undefined;
-              const resolveChildWorkflowStartContext = job.resolveChildWorkflowExecutionStart
-                ? this.consumeContext(
-                    this.pendingCompletionContexts.childWorkflowStart,
-                    job.resolveChildWorkflowExecutionStart.seq
-                  )
-                : undefined;
-              const resolveSignalContext = job.resolveSignalExternalWorkflow
-                ? this.consumeContext(
-                    this.pendingCompletionContexts.signalWorkflow,
-                    job.resolveSignalExternalWorkflow.seq
-                  )
-                : undefined;
-              const resolveCancelContext = job.resolveRequestCancelExternalWorkflow
-                ? this.consumeContext(
-                    this.pendingCompletionContexts.cancelWorkflow,
-                    job.resolveRequestCancelExternalWorkflow.seq
-                  )
-                : undefined;
-
-              return {
-                ...job,
-                initializeWorkflow: job.initializeWorkflow
-                  ? {
-                      ...job.initializeWorkflow,
-                      arguments: await decodeOptional(
-                        this.codecs,
-                        job.initializeWorkflow.arguments,
-                        this.workflowContext
-                      ),
-                      headers: noopDecodeMap(job.initializeWorkflow.headers),
-                      continuedFailure: await decodeOptionalFailure(
-                        this.codecs,
-                        job.initializeWorkflow.continuedFailure,
-                        this.workflowContext
-                      ),
-                      memo: {
-                        ...job.initializeWorkflow.memo,
-                        fields: await decodeOptionalMap(
-                          this.codecs,
-                          job.initializeWorkflow.memo?.fields,
-                          this.workflowContext
-                        ),
-                      },
-                      lastCompletionResult: {
-                        ...job.initializeWorkflow.lastCompletionResult,
-                        payloads: await decodeOptional(
-                          this.codecs,
-                          job.initializeWorkflow.lastCompletionResult?.payloads,
-                          this.workflowContext
-                        ),
-                      },
-                      searchAttributes: job.initializeWorkflow.searchAttributes
-                        ? {
-                            ...job.initializeWorkflow.searchAttributes,
-                            indexedFields: job.initializeWorkflow.searchAttributes.indexedFields
-                              ? noopDecodeMap(job.initializeWorkflow.searchAttributes.indexedFields)
-                              : undefined,
-                          }
-                        : undefined,
-                    }
-                  : null,
-                queryWorkflow: job.queryWorkflow
-                  ? {
-                      ...job.queryWorkflow,
-                      arguments: await decodeOptional(this.codecs, job.queryWorkflow.arguments, this.workflowContext),
-                      headers: noopDecodeMap(job.queryWorkflow.headers),
-                    }
-                  : null,
-                doUpdate: job.doUpdate
-                  ? {
-                      ...job.doUpdate,
-                      input: await decodeOptional(this.codecs, job.doUpdate.input, this.workflowContext),
-                      headers: noopDecodeMap(job.doUpdate.headers),
-                    }
-                  : null,
-                signalWorkflow: job.signalWorkflow
-                  ? {
-                      ...job.signalWorkflow,
-                      input: await decodeOptional(this.codecs, job.signalWorkflow.input, this.workflowContext),
-                      headers: noopDecodeMap(job.signalWorkflow.headers),
-                    }
-                  : null,
-                resolveActivity: job.resolveActivity
-                  ? {
-                      ...job.resolveActivity,
-                      result: job.resolveActivity.result
-                        ? {
-                            ...job.resolveActivity.result,
-                            completed: job.resolveActivity.result.completed
-                              ? {
-                                  ...job.resolveActivity.result.completed,
-                                  result: await decodeOptionalSingle(
-                                    this.codecs,
-                                    job.resolveActivity.result.completed.result,
-                                    resolveActivityContext
-                                  ),
-                                }
-                              : null,
-                            failed: job.resolveActivity.result.failed
-                              ? {
-                                  ...job.resolveActivity.result.failed,
-                                  failure: await decodeOptionalFailure(
-                                    this.codecs,
-                                    job.resolveActivity.result.failed.failure,
-                                    resolveActivityContext
-                                  ),
-                                }
-                              : null,
-                            cancelled: job.resolveActivity.result.cancelled
-                              ? {
-                                  ...job.resolveActivity.result.cancelled,
-                                  failure: await decodeOptionalFailure(
-                                    this.codecs,
-                                    job.resolveActivity.result.cancelled.failure,
-                                    resolveActivityContext
-                                  ),
-                                }
-                              : null,
-                          }
-                        : null,
-                    }
-                  : null,
-                resolveChildWorkflowExecution: job.resolveChildWorkflowExecution
-                  ? {
-                      ...job.resolveChildWorkflowExecution,
-                      result: job.resolveChildWorkflowExecution.result
-                        ? {
-                            ...job.resolveChildWorkflowExecution.result,
-                            completed: job.resolveChildWorkflowExecution.result.completed
-                              ? {
-                                  ...job.resolveChildWorkflowExecution.result.completed,
-                                  result: await decodeOptionalSingle(
-                                    this.codecs,
-                                    job.resolveChildWorkflowExecution.result.completed.result,
-                                    resolveChildWorkflowExecutionContext
-                                  ),
-                                }
-                              : null,
-                            failed: job.resolveChildWorkflowExecution.result.failed
-                              ? {
-                                  ...job.resolveChildWorkflowExecution.result.failed,
-                                  failure: await decodeOptionalFailure(
-                                    this.codecs,
-                                    job.resolveChildWorkflowExecution.result.failed.failure,
-                                    resolveChildWorkflowExecutionContext
-                                  ),
-                                }
-                              : null,
-                            cancelled: job.resolveChildWorkflowExecution.result.cancelled
-                              ? {
-                                  ...job.resolveChildWorkflowExecution.result.cancelled,
-                                  failure: await decodeOptionalFailure(
-                                    this.codecs,
-                                    job.resolveChildWorkflowExecution.result.cancelled.failure,
-                                    resolveChildWorkflowExecutionContext
-                                  ),
-                                }
-                              : null,
-                          }
-                        : null,
-                    }
-                  : null,
-                resolveChildWorkflowExecutionStart: job.resolveChildWorkflowExecutionStart
-                  ? {
-                      ...job.resolveChildWorkflowExecutionStart,
-                      cancelled: job.resolveChildWorkflowExecutionStart.cancelled
-                        ? {
-                            ...job.resolveChildWorkflowExecutionStart.cancelled,
-                            failure: await decodeOptionalFailure(
-                              this.codecs,
-                              job.resolveChildWorkflowExecutionStart.cancelled.failure,
-                              resolveChildWorkflowStartContext
-                            ),
-                          }
-                        : null,
-                    }
-                  : null,
-                resolveNexusOperation: job.resolveNexusOperation
-                  ? {
-                      ...job.resolveNexusOperation,
-                      result: {
-                        completed: job.resolveNexusOperation.result?.completed
-                          ? await decodeOptionalSingle(
-                              this.codecs,
-                              job.resolveNexusOperation.result.completed,
-                              this.workflowContext
-                            )
-                          : null,
-                        failed: job.resolveNexusOperation.result?.failed
-                          ? await decodeOptionalFailure(
-                              this.codecs,
-                              job.resolveNexusOperation.result.failed,
-                              this.workflowContext
-                            )
-                          : null,
-                        cancelled: job.resolveNexusOperation.result?.cancelled
-                          ? await decodeOptionalFailure(
-                              this.codecs,
-                              job.resolveNexusOperation.result.cancelled,
-                              this.workflowContext
-                            )
-                          : null,
-                        timedOut: job.resolveNexusOperation.result?.timedOut
-                          ? await decodeOptionalFailure(
-                              this.codecs,
-                              job.resolveNexusOperation.result.timedOut,
-                              this.workflowContext
-                            )
-                          : null,
-                      },
-                    }
-                  : null,
-                resolveSignalExternalWorkflow: job.resolveSignalExternalWorkflow
-                  ? {
-                      ...job.resolveSignalExternalWorkflow,
-                      failure: await decodeOptionalFailure(
-                        this.codecs,
-                        job.resolveSignalExternalWorkflow.failure,
-                        resolveSignalContext
-                      ),
-                    }
-                  : null,
-                resolveRequestCancelExternalWorkflow: job.resolveRequestCancelExternalWorkflow
-                  ? {
-                      ...job.resolveRequestCancelExternalWorkflow,
-                      failure: await decodeOptionalFailure(
-                        this.codecs,
-                        job.resolveRequestCancelExternalWorkflow.failure,
-                        resolveCancelContext
-                      ),
-                    }
-                  : null,
-              };
-            })
-          )
-        : null,
-    }) as Decoded<T>;
+    await visit<coresdk.workflow_activation.IWorkflowActivation, SerializationContext | undefined>(
+      activation,
+      walkWorkflowActivation,
+      {
+        transformPayload: async (payload, context) => (await decode(this.codecs, [payload], context))[0]!,
+        transformPayloads: (payloads, context) => decode(this.codecs, payloads, context),
+        initialContext: this.workflowContext,
+        skipHeaders: true,
+        skipSearchAttributes: true,
+        limit: this.codecOperationLimit,
+        deriveContext: (message, typeName, context) => {
+          switch (typeName) {
+            case 'coresdk.workflow_activation.ResolveActivity':
+              return this.consumeContext(
+                this.pendingCompletionContexts.activity,
+                (message as coresdk.workflow_activation.IResolveActivity).seq
+              );
+            case 'coresdk.workflow_activation.ResolveChildWorkflowExecution':
+              return this.consumeContext(
+                this.pendingCompletionContexts.childWorkflowComplete,
+                (message as coresdk.workflow_activation.IResolveChildWorkflowExecution).seq
+              );
+            case 'coresdk.workflow_activation.ResolveChildWorkflowExecutionStart':
+              return this.consumeContext(
+                this.pendingCompletionContexts.childWorkflowStart,
+                (message as coresdk.workflow_activation.IResolveChildWorkflowExecutionStart).seq
+              );
+            case 'coresdk.workflow_activation.ResolveSignalExternalWorkflow':
+              return this.consumeContext(
+                this.pendingCompletionContexts.signalWorkflow,
+                (message as coresdk.workflow_activation.IResolveSignalExternalWorkflow).seq
+              );
+            case 'coresdk.workflow_activation.ResolveRequestCancelExternalWorkflow':
+              return this.consumeContext(
+                this.pendingCompletionContexts.cancelWorkflow,
+                (message as coresdk.workflow_activation.IResolveRequestCancelExternalWorkflow).seq
+              );
+            default:
+              return context;
+          }
+        },
+      }
+    );
+    return activation as unknown as Decoded<T>;
   }
 
   /**
