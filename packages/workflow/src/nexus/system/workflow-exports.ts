d1ca3f369df533f199a14052b4a5f4687d3f9e89
Tim Conley <timothy.conley@temporal.io>
2026-09-04 16:32:08 -0700

    Remove System Nexus workflow export shim

--- packages/workflow/src/nexus/system/workflow-exports.ts
+++
@@ -1,7 +0,0 @@
-// Local workflow API proxy used by generated System Nexus bindings. Keep this
-// narrow so generated operation exports do not create an index-module cycle.
-export type { SignalDefinition } from '@temporalio/common';
-export { createNexusServiceClient } from '../../nexus';
-export type { NexusOperationHandle } from '../../nexus';
-export { getExternalWorkflowHandle, workflowInfo } from '../../workflow';
-export type { ExternalWorkflowHandle } from '../../workflow-handle';
