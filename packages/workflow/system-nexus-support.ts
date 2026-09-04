1379f6e86ed08f365cf5c264b23c786825deeb64
Tim Conley <timothy.conley@temporal.io>
2026-09-04 13:30:36 -0700

    Complete System Nexus serialization handling

--- packages/workflow/system-nexus-support.ts
+++ packages/workflow/system-nexus-support.ts
@@ -85,7 +85,7 @@ export function signalWithStartWorkflowSerializationContext(
 ): common.WorkflowSerializationContext {
   return {
     type: 'workflow',
-    namespace: workflow.workflowInfo().namespace,
+    namespace: request.namespace ?? workflow.workflowInfo().namespace,
     workflowId: request.id,
   };
 }
