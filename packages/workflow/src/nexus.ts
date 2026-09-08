670790138cbaf549bbc53c54db5442b11ad8172a
Tim Conley <timothy.conley@temporal.io>
2026-09-08 13:29:07 -0700

    Restore generated System Nexus interceptor dispatch

--- packages/workflow/src/nexus.ts
+++ packages/workflow/src/nexus.ts
@@ -18,7 +18,7 @@ import type { coresdk } from '@temporalio/proto';
 import { eventGroupMarkersToProto } from './event-groups';
 import { systemNexusOperationDefinition } from './nexus/system/payload-converter';
 import { withSystemNexusUserPayloadConverter } from './nexus/system/user-payload-converter';
-import { systemNexusSpecificInterceptorAdapters } from './nexus/system/generated/interceptors';
+import { dispatchSystemNexusSpecificInterceptors } from './nexus/system/generated/interceptors';
 import { CancellationScope } from './cancellation-scope';
 import { getActivator } from './global-attributes';
 import { composeInterceptors } from './interceptor-composition';
@@ -232,23 +232,24 @@ async function startSystemNexusOperationWithSpecificInterceptors<Output>(
     startSystemNexusOperationNextHandler
   );
 
-  const makeHandle = async (request: unknown): Promise<NexusOperationHandle<unknown>> => {
+  const makeHandle = async <Request, Result>(request: Request): Promise<NexusOperationHandle<Result>> => {
     const { token, result } = await generic({ ...input, input: request });
     return {
       service: input.service,
       operation: input.operation,
       token,
-      async result(): Promise<unknown> {
-        return await result;
+      async result(): Promise<Result> {
+        return (await result) as Result;
       },
     };
   };
-  const execute = composeInterceptors(
-    systemNexusSpecificInterceptorAdapters(input.service, input.operation, activator.interceptors.outbound),
-    'start',
+  return (await dispatchSystemNexusSpecificInterceptors(
+    input.service,
+    input.operation,
+    activator.interceptors.outbound,
+    input.input,
     makeHandle
-  );
-  return (await execute(input.input)) as NexusOperationHandle<Output>;
+  )) as NexusOperationHandle<Output>;
 }
 
 function startSystemNexusOperationNextHandler({
