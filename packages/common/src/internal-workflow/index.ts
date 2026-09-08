6801e1d468321794acd5a2855229aab344a52c0c
Tim Conley <timothy.conley@temporal.io>
2026-09-08 11:27:39 -0700

    Exclude generated System Nexus bindings from prune

--- packages/common/src/internal-workflow/index.ts
+++ packages/common/src/internal-workflow/index.ts
@@ -1,5 +1,13 @@
 export * from './enums-helpers';
-export * from './system-nexus';
+// These are consumed through the internal-workflow package entry point.
+// ts-prune-ignore-next
+export { SYSTEM_NEXUS_PAYLOAD_METADATA_KEY } from './system-nexus';
+// ts-prune-ignore-next
+export { SYSTEM_NEXUS_PAYLOAD_METADATA_VALUE } from './system-nexus';
+// ts-prune-ignore-next
+export { SYSTEM_NEXUS_CONTEXT_METADATA_KEY } from './system-nexus';
+// ts-prune-ignore-next
+export { TEMPORAL_SYSTEM_NEXUS_ENDPOINT } from './system-nexus';
 export {
   filterNullAndUndefined,
   mergeObjects,
